f1c584ce08d0c3cd17b4b5633005c54724518f1e
Nocxxecru <nicohoarau74@gmail.com>
2020-01-30 16:02:44 +0100

    base de données

---
+++ config/config.inc.php
@@ -0,0 +1,9 @@
+<?php
+// Le Type de la base de donnée
+define ('EDB_DBTYPE', "mysql");
+// Connexion local
+define('EDB_PORT', "3306");
+define('EDB_HOST', 'localhost');
+define('EDB_DBNAME', 'portfolio_m152');
+define('EDB_USER', 'root');
+define('EDB_PASS', 'Super');
