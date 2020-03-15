ddc33c709c840f04825b277e26718773bca9f559
Nocxxecru <nicolas.hr@eduge.ch>
2020-03-15 19:11:33 +0100

    correction condition type de fichier

--- App/php/sendPosts.php
+++ App/php/sendPosts.php
@@ -8,9 +8,10 @@ if (isset($_FILES['medias'])) {
   $finfo = finfo_open(FILEINFO_MIME_TYPE);
 
   for ($i = 0; $i < count($file['name']); $i++) {
-    $fileType = exif_imagetype($file['tmp_name'][$i]);
     $mimeType = finfo_file($finfo, $file['tmp_name'][$i]);
-    if (!image_type_to_mime_type($fileType) || $mimeType  == 'audio/mpeg' || explode('/', $mimeType)[0] == 'video/mp4') {
+    $fileType = explode('/', $mimeType)[0];
+
+    if ($fileType != 'image' || $fileType  != 'audio' || $fileType != 'video') {
       if ($file['size'][$i] > FILESIZE_MAX && !image_type_to_mime_type($fileType)) {
         echo json_encode([
           'ReturnCode' => 2,
@@ -34,6 +35,12 @@ if (isset($_FILES['medias'])) {
       'Success' => "Post send correctly"
     ]);
     exit();
+  } else {
+    echo json_encode([
+      'ReturnCode' => 4,
+      'Error' => "Error during the insert"
+    ]);
+    exit();
   }
 }
 
@@ -43,6 +50,12 @@ if (InsertPost($content)) {
   echo json_encode([
     'ReturnCode' => 0,
     'Success' => "Post send correctly"
+  ]);
+  exit();
+} else {
+  echo json_encode([
+    'ReturnCode' => 4,
+    'Error' => "Error during the insert"
   ]);
   exit();
 }
